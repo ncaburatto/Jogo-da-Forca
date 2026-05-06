df8d76569e2f05e804fb68d1ad174995e1da18dc
Nadkelly Buratto <kburratto@gmail.com>
2026-05-06 22:22:12 +0100

    Implementar o Jogo
    Desenvolvido o núcleo funcional do jogo

--- jogo.py
+++ jogo.py
@@ -1,1 +1,39 @@
+import random
 
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavras = ["PYTHON", "PROGRAMACAO", "DESENVOLVIMENTO"]
+    palavra_secreta = random.choice(palavras).upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ").strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou! Faltam {6 - erros} tentativas.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+
+if(__name__ == "__main__"):
+    jogar()
